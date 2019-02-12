b7adc2e6f9031c638f66784793b9d780cf14db79
Yuki Sumida <y.sumida.0201@gmail.com>
2019-02-12 22:36:06 +0900

    ボタンにidを付与して呼び出す

--- app/src/main/java/com/example/y_sumida/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/y_sumida/diceroller/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.y_sumida.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var rollButton = findViewById<Button>(R.id.roll_button)
+        rollButton.text = "Let's Roll"
     }
 }
